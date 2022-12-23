b62f67d9538a17c0f8acf3b64eaf88170beac650
Eduardo Klein Diefenthaler <120498575+eduardo-klein-dev@users.noreply.github.com>
2022-12-23 14:45:11 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Taxi</title>
+    <link rel="icon" href="img/logo.webp">
+
+    <!-- Links de Estilo: -->
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+<body>
+
+    <header>
+        <div class="container">
+            <div class="cabecalho">
+                <div class="logo">
+                    <a href="index.php"><img src="img/logo.png" alt="Logo da Página"></a>
+                </div>
+                <nav>
+                    <div class="menu">
+                        <div class="menu-item"></div>
+                        <div class="menu-item"></div>
+                        <div class="menu-item"></div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <main>
+
+        <section class="banner">
+            <div class="banner-body">
+                <h1 class="banner-titulo">BOOK YOUR TAXI NOW!</h1>
+                <h1 class="banner-subtitulo">+123 4567 8900</h1>
+            </div>
+        </section>
+
+        <section class="area1-fundo">
+            <img src="img/principal.png" alt="Foto Principal Táxi">
+            <div class="container area1">
+                <div class="area1-body">
+                    <div class="area1-text">
+                        <h5 class="area1-text-subtitulo">BEST IN CITY</h5>
+                        <h3 class="area1-text-titulo">To Book Taxi Please Use The Quick Reservation Form</h3>
+                        <p class="area1-text-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed aliquet justo. Quisque ut mollis est. Morbi sed laoreet enim. Donec eget tortor nulla. Ut dolor leo, convallis eget ipsum et, rutrum ornare sem. Sed volutpat commodo elit in rutrum. Donec fermentum facilisis odio, sit amet accumsan sapien placerat vel.</p>
+                        <a href="#">Button</a>
+                    </div>
+                    <div class="area1-form">
+                        <form action="" method="POST">
+                            <input type="text" placeholder="Enter your Name">
+                            <input type="text" placeholder="Enter your phone (exe. +14155552675)">
+                            <textarea id="area1-form-desc" type="textarea" placeholder="Enter your message"></textarea>
+                            <a type="submit">Submit</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="area2">
+            <div class="container area2-body">
+                <div class="area2-body-left">
+                    <div>
+                        <img src="img/smart.png" alt="">
+                    </div>
+                    <div class="area-text">
+                        Book and manage trips via our mobile apps.
+                    </div>
+                </div>
+                <div class="area2-body-right">
+                    <div><img src="img/appstore.png" alt="Donwload App Store"></div>
+                    <div><img src="img/googleplay.png" alt="Donwload Google Play"></div>
+                </div>
+            </div>
+        </section>
+
+        <section class="container">
+            <div class="area3-body">
+                <h2 class="area3-body-titulo">How Does It Work</h2>
+                <div class="area3-body-itens">
+                    <div class="area3-body-item-left">
+                        <img src="img/desenhotaxi.png" alt="">
+                        <h3 class="area3-body-iten-text">Enter your pickup point and time, select your car.</h3>
+                    </div>
+                    <div class="area3-body-item-center">
+                        <img src="img/desenhopagamento.png" alt="">
+                        <h3 class="area3-body-iten-text">Select your preferred payment method.</h3>
+                    </div>
+                    <div class="area3-body-item-right">
+                        <img src="img/desenho policial.png" alt="">
+                        <h3 class="area3-body-iten-text">Our taxi driver picks you up at the given location and time.</h3>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="area4">
+            <div class="area4-fundo">
+                <div class="container">
+                    <h5 class="area4-subtitulo">WE DO THE BEST</h5>
+                    <h1 class="area4-titulo">What You Wish</h1>
+                    <p class="area4-text">Duis ornare sapien at arcu rutrum, ut commodo magna vulputate</p>
+                    <div class="area4-items">
+                        <div class="area4-item">
+                            <div class="area4-item-icon">
+                                <img src="img/home.png" alt="">
+                                <h5 class="area4-item-titulo">HOME PICKUP</h5>
+                            </div>
+                            <p class="area4-item-text">Mauris porta, nisi a porttitor varius, ipsum lorem condimentum leo, laoreet faucibus nisl nunc finibus felis. Integer in sapien pulvinar, placerat ante ac, imperdiet ante. </p>
+                        </div>
+                        <div class="area4-item-right">
+                            <div class="area4-item-icon">
+                                <img src="img/money.png" alt="">
+                                <h5 class="area4-item-titulo">GPS SEARCHING</h5>
+                            </div>
+                            <p class="area4-item-text-right">Mauris porta, nisi a porttitor varius, ipsum lorem condimentum leo, laoreet faucibus nisl nunc finibus felis. Integer in sapien pulvinar, placerat ante ac, imperdiet ante. </p>
+                        </div>
+                        <div class="area4-item">
+                            <div class="area4-item-icon">
+                                <img src="img/gps.png" alt="">
+                                <h5 class="area4-item-titulo">BONUSES FOR RIDE</h5>
+                            </div>
+                            <p class="area4-item-text">Mauris porta, nisi a porttitor varius, ipsum lorem condimentum leo, laoreet faucibus nisl nunc finibus felis. Integer in sapien pulvinar, placerat ante ac, imperdiet ante. </p>
+                        </div>
+                        <div class="area4-item-right">
+                            <div class="area4-item-icon">
+                                <img src="img/run.png" alt="">
+                                <h5 class="area4-item-titulo">FAST BOOKING</h5>
+                            </div>
+                            <p class="area4-item-text-right">Mauris porta, nisi a porttitor varius, ipsum lorem condimentum leo, laoreet faucibus nisl nunc finibus felis. Integer in sapien pulvinar, placerat ante ac, imperdiet ante. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="area5">
+            <div class="container area5-body">
+                <div class="area5-body-item">
+                    <h3 class="area5-body-item-titulo">Taxi</h3>
+                    <img class="img-taxi" src="img/taxi.png" alt="">
+                    <h5 class="area5-body-item-titulo-lista">The Taxi You Know</h5>
+                    <ul class="area5-body-item-titulo-lista-desc-left">
+                        <li>Price based on taximeter</li>
+                        <li>Executed by local taxi fleets</li>
+                        <li>Online and offline payment</li>
+                        <li>24/7 support hotline</li>
+                    </ul>
+                </div>
+                <div class="area5-body-item">
+                    <h3 class="area5-body-item-titulo">Economy+</h3>
+                    <img src="img/taxieconomy.png" alt="">
+                    <h5 class="area5-body-item-titulo-lista">For Budget Travelers</h5>
+                    <ul class="area5-body-item-titulo-lista-desc-center">
+                        <li>Fixed price</li>
+                        <li>English-speaking driver</li>
+                        <li>Automated flight and train tracking</li>
+                        <li>45 minutes waiting time after landing</li>
+                        <li>Pickup with name sign in arrivals hall</li>
+                        <li>Children seats available on request</li>
+                        <li>Online and offline payment</li>
+                        <li>24/7 support hotline</li>
+                    </ul>
+                </div>
+                <div class="area5-body-item">
+                    <h3 class="area5-body-item-titulo">Business Class</h3>
+                    <img src="img/taxiclass.png" alt="">
+                    <h5 class="area5-body-item-titulo-lista">Business Traveler's Favourite</h5>
+                    <ul class="area5-body-item-titulo-lista-desc-right">
+                        <li>Black car guaranteed</li>
+                        <li>Fixed price</li>
+                        <li>English-speaking driver</li>
+                        <li>Automated flight and train tracking</li>
+                        <li>45 minutes waiting time after landing</li>
+                        <li>Pickup with name sign in arrivals hall</li>
+                        <li>Children seats available on request</li>
+                        <li>Online and offline payment</li>
+                        <li>24/7 support hotline</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+        <section class="area6">
+            <div class="area6-fundo">
+                <div class="container">
+                    <h5 class="area6-subtitulo">HAPPY CLIENTS</h5>
+                    <h1 class="area6-titulo">User's Testimonials</h1>
+                    <div class="area6-body">
+                        <div class="area6-body-left">
+                            <div class="area6-body-left-img">
+                                <img src="img/mulher.png" alt="">
+                            </div>
+                            <p class="area6-body-left-text">his was one of the best experiences I have ever had with a cab company. I had problems at the airport and the driver stayed with me for over an hour and helped me sort everything out.</p>
+                            <h5 class="area6-name-1">POLLY WALTER</h5>
+                        </div>
+                        <div class="area6-body-right">
+                            <div class="area6-body-right-img">
+                                <img src="img/homem.png" alt="">
+                            </div>
+                            <p class="area6-body-right-text">Thank you for such fabulous service! Mauris porta, nisi a porttitor varius, ipsum lorem condimentum leo, laoreet faucibus nisl nunc finibus felis. Integer in sapien pulvinar.</p>
+                            <h5 class="area6-name-2">CONNOR QUINN</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="area7">
+            <div class="container">
+                <h2 class="subtitulo-area7">WE ARE READY TO TAKE OUR CALL 24 HOURS, 7 DAYS!</h2>
+                <h1 class="titulo-area7">+123 4567 8900</h1>
+            </div>
+        </section>
+
+        <footer class="rodape">
+            <div class="container">
+                <h1 class="rodape-titulo">How I Can Pay</h1>
+                <div class="rodape-body">
+                    <div class="rodape-body-left">
+                        <h5 class="rodape-body-left-titulo">Individual Customers</h5>
+                        <p class="rodape-body-left-text">We accepts all major payment methods including MasterCard, Visa and American Express.</p>
+                    </div>
+                    <div class="rodape-body-right">
+                        <h5 class="rodape-body-right-titulo">Corporate Customers</h5>
+                        <p class="rodape-body-right-text">We supports corporate payment methods such as AirPlus. Upon special request, payment by invoice can be made available for our partners and event organisers.</p>
+                    </div>
+                </div>
+                <div class="roadape-socials">
+                    <div class="rodape-social"><a class="link" href="#"><i class="fa-brands fa-facebook-f"></a></i></div>
+                    <div class="rodape-social"><a class="link" href="#"><i class="fa-brands fa-twitter"></i></a></div>
+                    <div class="rodape-social"><a class="link" href="#"><i class="fa-brands fa-instagram"></i></a></div>
+                    <div class="rodape-social"><a class="link" href="#"><i class="fa-brands fa-linkedin-in"></i></a></div>
+                </div>
+                <div class="rodape-creditos">
+                    <h5>Template gerado por <a class="link" href="https://nicepage.com/"><u>Nicepage</u></a></h5>
+                    <h5>Código fonte programado por <a class="link" href="https://github.com/eduardo-klein-dev"><u>@eduardo-klein-dev</a></u></h5>
+                </div>
+            </div>
+        </footer>
+
+    </main>
+
+    <!-- Links JS: -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
